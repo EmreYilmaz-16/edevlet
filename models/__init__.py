f7319422caab1b469a0372087d977124afe52f5c
Emre YILMAZ <40140052+EmreYilmaz-16@users.noreply.github.com>
2026-02-10 12:34:10 +0300

    Add invoice XML/XSLT preview wizard in Odoo

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
 from . import edevletintegration
 from . import einvoice
 from . import account_move
+from . import invoice_xml_preview_wizard
 from . import account_tax_group
 from . import res_partner
